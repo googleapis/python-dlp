451682ea99afdd60dea859853cf41b8cdaeca927
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-14 19:30:23 -0400

    chore: prevent normalization of semver versioning

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ if "google.cloud" in packages:
 
 setuptools.setup(
     name=name,
-    version=version,
+    version=setuptools.sic(version),
     description=description,
     long_description=readme,
     author="Google LLC",
